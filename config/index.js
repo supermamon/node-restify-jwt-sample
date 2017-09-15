d27fe7f55624426f916c8289f1d17918df331f61
Raymond Velasquez <at.supermamon@gmail.com>
2017-09-15 11:47:12 +0800

    Change: removed url-join dependency

--- config/index.js
+++ config/index.js
@@ -3,8 +3,6 @@
 // a workaround since restify does not have this yet
 const API_ROOT  = '/api'
 
-const urljoin   = require('url-join')
-
 module.exports = {
     LOG_LEVEL   : process.env['LOG_LEVEL'] || 'info',
     PORT        : process.env['PORT'] || 8080,
@@ -13,8 +11,8 @@ module.exports = {
     JWT_SECRET  : 'some-secret',
 
     // will be used to building route paths
-    basePath    : function(path) {
-        return urljoin(API_ROOT, path);
+    basePath    : (path) => {
+        return API_ROOT.replace(/\/$/,'') + '/' + path.replace(/^\//,'')
     }
     
 }
